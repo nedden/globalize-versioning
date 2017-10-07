035d07f8ae8c6ec55b729dfedddc0db2138d6407
Denis N <nedden@gmail.com>
2017-10-07 11:04:38 +0530

    rails 5 compatibility

--- lib/globalize/versioning.rb
+++ lib/globalize/versioning.rb
@@ -23,5 +23,6 @@ Globalize::ActiveRecord::ActMacro.module_eval do
     end
   end
 
-  alias_method_chain :setup_translates!, :versioning
+  alias_method :versioning, :setup_translates!
+  alias_method :setup_translates!, :versioning
 end
